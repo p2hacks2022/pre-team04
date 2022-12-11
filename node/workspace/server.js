7794f8c78a895d6c99136612328c7ca82c391643
sumeshi96 <yumapc96@gmail.com>
2022-12-12 01:18:25 +0900

    update: 各ページヘのHTTPリクエストに対してレスポンスを返すことができた

--- node/workspace/server.js
+++ node/workspace/server.js
@@ -2,8 +2,32 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send("Hello,world");
+    res.send("This is login page !");
+});
+
+app.get('/login', (req, res) => {
+    res.send("This is login page !");
+});
+
+app.get('/home', (req, res) => {
+    res.send("This is home page !");
+});
+
+app.get('/register', (req, res) => {
+    res.send("This is register page !");
+});
+
+app.get('/record', (req, res) => {
+    res.send("This is record page !");
+});
+
+app.get('/manage', (req, res) => {
+    res.send("This is manage page !");
 });
 
 app.listen(port, () => {

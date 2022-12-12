7a8cfcb0d1e9799ad3c05d09dcc5685d297626e1
sumeshi96 <yumapc96@gmail.com>
2022-12-13 05:01:51 +0900

    add: ejsファイルの参照先を記述（まだフロントができてないのでコメントアウト）

--- node/workspace/server.js
+++ node/workspace/server.js
@@ -8,33 +8,15 @@ app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
 
+// ルートへのアクセスはログインへリダイレクト
 app.get('/', (req, res) => {
     res.redirect(301, '/login');
 });
 
+// ログイン画面
 app.get('/login', (req, res) => {
     res.send("This is login page !");
-});
-
-app.get('/addNewUser', (req, res) => {
-    res.send("This is newUser page!");
-});
-
-// ユーザーの新規登録
-app.post('/addNewUser/add', (req, res) => {
-    let addingUserId = req.body.userId;
-    if (!(fs.existsSync(`./data/${addingUserId}.json`))) {
-        fs.writeFileSync(`./data/${addingUserId}.json`, `{"userId": "${addingUserId}"}`, (err) => {
-            if (err) {
-                throw err;
-            };
-        });
-        res.header('Content-Type', 'application/json; charset=utf-8');
-        res.send(`{"message":"ユーザー「${addingUserId}」を登録しました！"}`);
-    } else {
-        res.header('Content-Type', 'application/json; charset=utf-8');
-        res.send(`{"message":"ユーザー「${addingUserId}」はすでに存在しています！"}`);
-    }
+    // res.render("./view/pages/login.ejs");
 });
 
 // ログイン認証
@@ -54,37 +36,71 @@ app.post('/login/auth', (req, res) => {
     }
 });
 
+// ユーザーの新規登録画面
+app.get('/addNewUser', (req, res) => {
+    res.send("This is newUser page!");
+    // res.render("./view/pages/addNewUser.ejs");
+});
 
+// ユーザーの新規登録
+app.post('/addNewUser/add', (req, res) => {
+    let addingUserId = req.body.userId;
+    if (!(fs.existsSync(`./data/${addingUserId}.json`))) {
+        fs.writeFileSync(`./data/${addingUserId}.json`, `{"userId": "${addingUserId}"}`, (err) => {
+            if (err) {
+                throw err;
+            };
+        });
+        res.header('Content-Type', 'application/json; charset=utf-8');
+        res.send(`{"message":"ユーザー「${addingUserId}」を登録しました！"}`);
+    } else {
+        res.header('Content-Type', 'application/json; charset=utf-8');
+        res.send(`{"message":"ユーザー「${addingUserId}」はすでに存在しています！"}`);
+    }
+});
+
+// ホーム画面
 app.get('/:user/home', (req, res) => {
     res.send(`This is ${req.params.user} home page !`);
+    // res.render("./view/pages/home.ejs");
 });
 
+// 登録画面
 app.get('/:user/register', (req, res) => {
     res.send("This is register page !");
+    // res.render("./view/pages/register.ejs");
 });
 
+// 登録リクエスト 
 app.post('/:user/register/add', (req, res) => {
     res.send(`${req.body.goal},${req.body.newCategory},${req.body.selectCategory}`);
     // userのjsonに追加する
 });
 
+// 記録画面
 app.get('/:user/record', (req, res) => {
     res.send("This is record page !");
+    // res.render("./view/pages/record.ejs");
 });
 
+// 記録リクエスト
 app.post('/:user/record/add', (req, res) => {
     res.send(`${req.body.goal},${req.body.doneAny},${req.body.achivementDegrees}`);
     // userのjsonに追加する
 });
 
+// 管理画面
 app.get('/:user/manage', (req, res) => {
     res.send("This is manage page !");
+    // res.render("./view/pages/manage.ejs");
 });
 
+// カテゴリフィルター
 app.post('/:user/manage/select', (req, res) => {
     res.send(`${req.body.selectCategory}`);
     // userのjsonをカテゴリを絞って表示する
 });
+
 
 app.listen(port, () => {
     console.log(`upDate listening on port ${port}`);

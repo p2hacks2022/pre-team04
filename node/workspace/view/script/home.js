58c11514ace0db8d3c8290ddf5749287055b2a46
sumeshi96 <yumapc96@gmail.com>
2022-12-17 02:50:21 +0900

    add: ボタンを押下されたときの処理の雛形を作った

---
+++ node/workspace/view/script/home.js
@@ -0,0 +1,47 @@
+// const = document.getElementById();
+
+// headerの要素取得
+const headerLogoContainer = document.getElementById(headerLogoButton);
+const headerNaviRegisterBox = document.getElementById(headerNaviRegisterButton);
+const headerNaviRecordBox = document.getElementById(headerNaviRecordButton);
+const headerNaviManageBox = document.getElementById(headerNaviManageButton);
+
+// bodyの要素の取得
+const homeRegisterButton = document.getElementById(registerButton);
+const homeRecordButton = document.getElementById(recordButton);
+const homeManageButton = document.getElementById(manageButton);
+
+// logoを押下されたらhomeへ遷移
+headerLogoContainer.addEventListener('click', async () => {
+    console.log("移動しますよ");
+});
+
+// naviの登録を押下されたらregisterへ遷移
+headerNaviRegisterBox.addEventListener('click', async () => {
+    console.log("移動しますよ");
+});
+
+// naviの記録を押下されたらrecordへ遷移
+headerNaviRecordBox.addEventListener('click', async () => {
+    console.log("移動しますよ");
+});
+
+// naviの管理を押下されたらmanageへ遷移
+headerNaviManageBox.addEventListener('click', async () => {
+    console.log("移動しますよ");
+});
+
+// registerボタンを押下されたらregisterへ遷移
+homeRegisterButton.addEventListener('click', async () => {
+    console.log("移動しますよ");
+});
+
+// recordボタンを押下されたらrecordへ遷移
+homeRecordButton.addEventListener('click', async () => {
+    console.log("移動しますよ");
+});
+
+// manageボタンを押下されたらmanageへ遷移
+homeManageButton.addEventListener('click', async () => {
+    console.log("移動しますよ");
+});

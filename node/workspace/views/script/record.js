8f046ac1cdc58ba705651ea7036931d0ad6beb5d
sumeshi96 <yumapc96@gmail.com>
2022-12-17 23:13:05 +0900

    fix: viewをviewsに修正

---
+++ node/workspace/views/script/record.js
@@ -0,0 +1,14 @@
+
+// headerの要素取得
+const headerLogoContainer = document.getElementById(headerLogoButton);
+const headerNaviRegisterBox = document.getElementById(headerNaviRegisterButton);
+const headerNaviRecordBox = document.getElementById(headerNaviRecordButton);
+const headerNaviManageBox = document.getElementById(headerNaviManageButton);
+
+//bodyの要素取得
+const recordTargetBox = document.getElementById(recordTargetBox);
+const recordDoneAnyForm = document.getElementById(doneAnyForm);
+const recordAchievementDegreesBox = document.getElementById(achievementDegreesBox);
+const recordButton = document.getElementById(recordButton);
+const recordBackHomeButton = document.getElementById(backHomeButton);
+const recordSwitchManageMenuButton = document.getElementById(switchManageMenuButton);

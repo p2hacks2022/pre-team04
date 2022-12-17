793ae880f3234644daa9854f4505a5eca5280b7d
sumeshi96 <yumapc96@gmail.com>
2022-12-18 01:46:19 +0900

    add: homeから各ページに移動できるようにした

--- node/workspace/views/script/home.js
+++ node/workspace/views/script/home.js
@@ -1,50 +1,61 @@
 // const = document.getElementById();
 
 // headerの要素取得
-const headerLogoContainer = document.getElementById(headerLogoButton);
-const headerNaviRegisterBox = document.getElementById(headerNaviRegisterButton);
-const headerNaviRecordBox = document.getElementById(headerNaviRecordButton);
-const headerNaviManageBox = document.getElementById(headerNaviManageButton);
+const headerLogoContainer = document.getElementById('headerLogoButton');
+const headerNaviRegisterBox = document.getElementById('headerNaviRegisterButton');
+const headerNaviRecordBox = document.getElementById('headerNaviRecordButton');
+const headerNaviManageBox = document.getElementById('headerNaviManageButton');
 
 // bodyの要素の取得
-const homeRegisterButton = document.getElementById(registerButton);
-const homeRecordButton = document.getElementById(recordButton);
-const homeManageButton = document.getElementById(manageButton);
-
-let path = location.pathname.split("/");
-let userId = path[0];
+const homeRegisterButton = document.getElementById('registerButton');
+const homeRecordButton = document.getElementById('recordButton');
+const homeManageButton = document.getElementById('manageButton');
 
 // logoを押下されたらhomeへ遷移
-headerLogoContainer.addEventListener('click', async () => {
+headerLogoContainer.addEventListener('click', () => {
+    let path = location.pathname.split("/");
+    let userId = path[1];
     window.location.href = `http://update.com/${userId}/home`
 });
 
 // naviの登録を押下されたらregisterへ遷移
-headerNaviRegisterBox.addEventListener('click', async () => {
+headerNaviRegisterBox.addEventListener('click', () => {
+    let path = location.pathname.split("/");
+    let userId = path[1];
     window.location.href = `http://update.com/${userId}/register`
 });
 
 // naviの記録を押下されたらrecordへ遷移
-headerNaviRecordBox.addEventListener('click', async () => {
+headerNaviRecordBox.addEventListener('click', () => {
+    let path = location.pathname.split("/");
+    let userId = path[1];
     window.location.href = `http://update.com/${userId}/record`
 });
 
 // naviの管理を押下されたらmanageへ遷移
-headerNaviManageBox.addEventListener('click', async () => {
+headerNaviManageBox.addEventListener('click', () => {
+    let path = location.pathname.split("/");
+    let userId = path[1];
     window.location.href = `http://update.com/${userId}/manage`
 });
 
 // registerボタンを押下されたらregisterへ遷移
-homeRegisterButton.addEventListener('click', async () => {
+homeRegisterButton.addEventListener('click', () => {
+    let path = location.pathname.split("/");
+    let userId = path[1];
     window.location.href = `http://update.com/${userId}/register`
 });
 
 // recordボタンを押下されたらrecordへ遷移
-homeRecordButton.addEventListener('click', async () => {
+homeRecordButton.addEventListener('click', () => {
+    let path = location.pathname.split("/");
+    let userId = path[1];
     window.location.href = `http://update.com/${userId}/record`
 });
 
 // manageボタンを押下されたらmanageへ遷移
-homeManageButton.addEventListener('click', async () => {
+homeManageButton.addEventListener('click', () => {
+    let path = location.pathname.split("/");
+    let userId = path[1];
     window.location.href = `http://update.com/${userId}/manage`
 });
